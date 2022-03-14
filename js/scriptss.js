a9428e4ebb7b04cbdfbc90d2035c70f804cab216
KirillKirpikin <101583493+KirillKirpikin@users.noreply.github.com>
2022-03-14 19:09:07 +0200

    food
    food

---
+++ js/scriptss.js
@@ -0,0 +1,50 @@
+'use strict';
+import tabs from './modules/tabs';
+import modal from './modules/modal';
+import timer from './modules/timer';
+import cardss from './modules/cardss';
+import calc from './modules/calc';
+import forms from './modules/forms';
+import slider from './modules/slider';
+import {openModal} from './modules/modal';
+
+window.addEventListener('DOMContentLoaded', () => {
+    const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 50000);
+    
+    tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
+    modal('[data-modal]', '.modal', modalTimerId);
+    timer('.timer', '2021-12-31');
+    cardss();
+    calc();
+    forms('form', modalTimerId);
+    slider({
+        container: '.offer__slider',
+        nextArrow: '.offer__slider-next',
+        prevArrow: '.offer__slider-prev',
+        slide: '.offer__slide',
+        totalCounter: '#total',
+        currentCounter: '#current',
+        wrapper: '.offer__slider-wrapper',
+        field: '.offer__slider-inner'
+
+    });
+});
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
